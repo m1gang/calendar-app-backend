2002a0b43840e868f054dc7d439aa8ac1da67d2f
MiGaNg <miguelangelyv1@gmail.com>
2026-03-25 16:35:02 -0500

    Creando las rutas relacionadas a usuarios

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ const app = express();
 app.use(express.static("public"));
 
 //Rutas
-// app.get("/", (req, res) => {
-//   res.json({
-//     ok: true,
-//   });
-// });
+app.use("/api/auth", require("./routes/auth"));
+// todo-> crud: Eventos
 
 // Escuchar peticiones
 
