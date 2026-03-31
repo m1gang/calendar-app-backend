4320cf5ea8bab3d60bea8e6f114779add2b6b585
MiGaNg <miguelangelyv1@gmail.com>
2026-03-31 17:51:49 -0500

    Express Validator

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@
 
 const { Router } = require("express");
 const router = Router();
+const { check } = require("express-validator");
 
 const {
   crearUsuario,
@@ -12,9 +13,30 @@ const {
   revalidarToken,
 } = require("../controllers/auth");
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    //midlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email no es válido").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  crearUsuario,
+);
 
-router.post("/", loginUsuario);
+router.post(
+  "/",
+  [
+    //midlewares
+    check("email", "El email no es válido").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  loginUsuario,
+);
 
 router.get("/renew", revalidarToken);
 
