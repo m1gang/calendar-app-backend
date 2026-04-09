95e8a5cc3962346a28a4aa7c71f6dcfaa780efa5
MiGaNg <miguelangelyv1@gmail.com>
2026-04-08 19:25:49 -0500

    Crear un usuario en nuestra Base de Datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,25 @@
 const { response } = require("express");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
+const crearUsuario = async (req, res = response) => {
+  // const { name, email, password } = req.body;
+  try {
+    const usuario = new Usuario(req.body);
 
-  res.status(201).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: "register",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
