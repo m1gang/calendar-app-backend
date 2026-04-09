e51f5a416824f09f024bf7b890d285551d953664
MiGaNg <miguelangelyv1@gmail.com>
2026-04-09 17:19:23 -0500

    Generar un Json Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
+const { generarJWT } = require("../helpers/jwt");
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
@@ -22,10 +23,14 @@ const crearUsuario = async (req, res = response) => {
 
     await usuario.save();
 
+    //Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
+
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     console.log(error);
@@ -60,11 +65,13 @@ const loginUsuario = async (req, res = response) => {
     }
 
     //Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
 
     res.json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     console.log(error);
@@ -73,13 +80,6 @@ const loginUsuario = async (req, res = response) => {
       msg: "Hable con el administrador",
     });
   }
-
-  res.json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
 };
 
 const revalidarToken = (req, res = response) => {
