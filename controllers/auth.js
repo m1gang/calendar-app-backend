df193d095f84428956c993349001600255533ba3
MiGaNg <miguelangelyv1@gmail.com>
2026-04-09 16:37:40 -0500

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,9 +6,7 @@ const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
   try {
-    let usuario = await Usuario.findOne({
-      email,
-    });
+    let usuario = await Usuario.findOne({ email });
     if (usuario) {
       return res.status(400).json({
         ok: false,
@@ -39,8 +37,42 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
+
+  try {
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe con ese email",
+      });
+    }
+
+    //Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+
+    //Generar JWT
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 
   res.json({
     ok: true,
