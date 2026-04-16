a82861575fba19ab06d9404437ef0f0683ab297e
MiGaNg <miguelangelyv1@gmail.com>
2026-04-16 16:04:36 -0500

    Validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,9 @@
 const { Router } = require("express");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const router = Router();
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { isDate } = require("../helpers/isDate");
 
 const {
   actualizarEvento,
@@ -21,7 +24,16 @@ router.use(validarJWT);
 router.get("/", getEventos);
 
 // Crear un nuevo evento
-router.post("/", crearEvento);
+router.post(
+  "/",
+  [
+    check("title", "El título es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalización es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  crearEvento,
+);
 
 // Actualizar evento
 router.put("/:id", actualizarEvento);
